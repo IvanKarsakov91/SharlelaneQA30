1c5b2dc8dee4fc866fd8e4bc010d0d8cd157eea0
IvanKarsakov <IvanKarsakov91@gmail.com>
2025-04-23 20:11:39 +0300

    added ZipCodeTest

--- src/test/java/ZipCodeTest.java
+++ src/test/java/ZipCodeTest.java
@@ -1,6 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class ZipCodeTest {
 
@@ -8,8 +13,24 @@ public class ZipCodeTest {
     public void checkZipCode4digits(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver browser = new ChromeDriver();
+        browser.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
         browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys( "1234");
+        browser.findElement(By.cssSelector("[value=Continue]")).click();
+        String actualErrorMessage = browser.findElement(By.className("errorMessage")).getText();
+        Assert.assertEquals(actualErrorMessage, "Oops, error on page. ZIP code should have 5 digits");
         browser.quit();
-
+    }
+    @Test
+    public void checkZipCodeEmpty(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys( "");
+        browser.findElement(By.cssSelector("[value=Continue]")).click();
+        String actualErrorMessage = browser.findElement(By.className("errorMessage")).getText();
+        Assert.assertEquals(actualErrorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
     }
 }
